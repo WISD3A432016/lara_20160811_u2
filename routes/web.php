51a0382ef7594d15defc68b52d2ead5ba0c4417f
Jing-Ru Sun <sun.3a432016@gmail.com>
2017-10-12 14:44:50 +0800

    Exercise 4-3 修改 Route 裡的路徑

--- routes/web.php
+++ routes/web.php
@@ -46,15 +46,24 @@ Route::get('ADD NEW/{Route}', function ($Route) {
 });
 */
 
-/*練習4：為 Route 命名 */
+/*練習4：為 Route 命名
 //Exercise 4-1(延用Exercise 2-3)
 Route::get('hello/{Ruby?}', ['as' => 'hello.index', function ($name = 'Everybody') {
     //Exercise 4-1
     return 'Hello, '  .$name;
 }]);
 
-/*'練習 4: 測試 hello.index的超連結*/
+
+/*'練習 4: 測試 hello.index的超連結
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+//Exercise 4-3 修改 Route 裡的路徑
+Route::get('say/{Ruby?}', ['as' => 'hello.index', function ($name = 'Everybody') {
+    return  view('welcome');
+}]);
+
+
 
